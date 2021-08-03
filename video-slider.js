5acdb957f106231acf888c595b1345129ba807ea
Maksim Putikov <maksim.putikov0@gmail.com>
2021-08-03 22:53:57 +0300

    fix

--- video-slider.js
+++ video-slider.js
@@ -1,43 +1,47 @@
-let videos = document.querySelectorAll('.video-controls-slider .video-dot');
+let vid = document.querySelector('.video-slider video-slide');
 let currentItem1 = 0;
 let isEnabled1 = true;
-function changeCurrentItem(n) {
-	currentItem = (n + videos.length) % videos.length;
-}
-function hideItem(direction) {
-	isEnabled1 = false;
-	videos[currentItem1].classList.add(direction);
-	videos[currentItem1].addEventListener('animationend', function() {
-		this.classList.remove('active', direction);
-	});
-}
-function showItem(direction) {
-	videos[currentItem].classList.add('next', direction);
-	videos[currentItem].addEventListener('animationend', function() {
-		this.classList.remove('next', direction);
-		this.classList.add('active');
-		isEnabled1 = true;
-	});
-}
-function nextItem(n) {
-	hideItem('to-left');
-	changeCurrentItem(n + 1);
-	showItem('from-right');
-}
-function previousItem(n) {
-	hideItem('to-right');
-	changeCurrentItem(n - 1);
-	showItem('from-left');
-}
-document.querySelector('.control.left').addEventListener('click', function() {
-	if (isEnabled) {
-		previousItem(currentItem);
 
-	}
-});
+function changeCurrentItem1(j){
+currentItem1 = (j+vid.length) % vid.length;
+}
 
-document.querySelector('.control.right').addEventListener('click', function() {
-	if (isEnabled) {
-		nextItem(currentItem);
-	}
-});
+function hideItem1(direction1){
+    isEnabled1 = false;
+    vid[currentItem1].classList.add(direction1);
+    vid[currentItem1].addEventListener('animationend', function(){
+        this.classList.remove('active1',direction1);
+    })
+}
+
+function showItem1(direction1){
+    vid[currentItem1].classList.add('next1',direction1)
+    vid[currentItem1].addEventListener('animationend', function(){
+        this.classList.remove('next1',direction1);
+        this.classList.add('active1');
+        isEnabled1 = true;
+    })
+}
+
+function nextItem1(j){
+    hideItem1('to-left1');
+    changeCurrentItem1(j+1);
+    showItem1('from-right1');
+}
+
+function previousItem1(j){
+    hideItem1('to-right1');
+    changeCurrentItem1(j-1);
+    showItem1('from-left1');
+}
+
+document.querySelector('.video-arrow.left').addEventListener('click', function(){
+    if (isEnabled1){
+        previousItem1(currentItem1)
+    }
+})
+document.querySelector('.video-arrow.right').addEventListener('click', function(){
+    if (isEnabled1){
+        nextItem1(previousItem1)
+    }
+})
